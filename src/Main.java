bb7a0aa027f2dc2413f29ea6a4bbff7fdd3f3fb2
Sobh6n <sobhankhedry7297@gmail.com>
2024-05-23 10:17:59 +0330

    working not done

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    static int triangle[][] = {{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}};
+    static int r[][]= new int[4][4];
+    static int s[][]= new int[4][4];
+    static int selected[][] = new int[8][8];
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int n = triangle.length;
+
+        System.out.println(MinimumSumPath(triangle, n,0,0));
+        for (int i =0 ; i<n; i++){
+            for (int j = 0; j < n; j++) {
+                if(selected[i][j]!= 0){
+                    System.out.println("index i: "+ i + "index j:"+ j);
+                    System.out.println(selected[i][j]+ " ");
+                }
+
+            }
+
+        }
+
     }
-}
-class Node{
-    int Value;
-    Node Left;
-    Node Right;
-}
+        public static int MinimumSumPath(int[][] triangle, int n,int i, int j){
+            if (i == n-1)
+            {
+                return triangle[i][j];
+            }
+            if(r[i][j]>0)
+            {
+                System.out.println("Here returned");
+                return r[i][j];
+            }
+            int q = MinimumSumPath(triangle,n,i+1,j) ;
+            int p = MinimumSumPath(triangle,n,i+1,j+1);
+            if (p>q)
+            {
+                r[i][j]=q + triangle[i][j] ;
+                 s[i][j]=triangle[i][j];
+
+                 selected[i][j]=triangle[i][j];
+                 return r[i][j];
+            }
+            else {
+                r[i][j] =p + triangle[i][j];
+                s[i][j]=triangle[i][j];
+
+                selected[i][j]= triangle[i][j];
+                return r[i][j];
+            }
+
+        }
+
+
+
+    }
