5a8f4cff02eeb69305b00abdb2b6b9580ad43632
Sobh6n <sobhankhedry7297@gmail.com>
2024-06-01 15:56:03 +0330

    done

--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,53 @@ import java.util.List;
 public class Main {
 
     static int triangle[][] = {{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}};
+
     static int r[][]= new int[4][4];
-    static int s[][]= new int[4][4];
-    static int selected[][] = new int[8][8];
+
+    static int selected[][] = new int[5][5];
 
 
     public static void main(String[] args) {
 
         int n = triangle.length;
 
+        System.out.println("Min Sum Path is : ");
         System.out.println(MinimumSumPath(triangle, n,0,0));
-        for (int i =0 ; i<n; i++){
-            for (int j = 0; j < n; j++) {
-                if(selected[i][j]!= 0){
-                    System.out.println("index i: "+ i + "index j:"+ j);
-                    System.out.println(selected[i][j]+ " ");
-                }
 
-            }
 
+        System.out.println();
+        System.out.println("The selected path");
+        PrintSelected(selected, 0 ,0);
+        System.out.println();
+
+
+    }
+
+    private static void PrintSelected(int[][] selected, int i, int j) {
+        if(selected[i][j] == -1){
+            System.out.println(triangle[i][j]);
+            PrintSelected(selected,i+1,j);
+        }
+        if (selected[i][j] == 1){
+            System.out.println(triangle[i][j]);
+            PrintSelected(selected, i+1, j+1);
+        }
+        if (selected[i][j]==10){
+            System.out.println(triangle[i][j]);
+            return;
         }
 
     }
-        public static int MinimumSumPath(int[][] triangle, int n,int i, int j){
+
+    public static int MinimumSumPath(int[][] triangle, int n,int i, int j){
             if (i == n-1)
             {
+                selected[i][j]=10;
                 return triangle[i][j];
             }
-            if(r[i][j]>0)
+            if(selected[i][j]!=0)
             {
-                System.out.println("Here returned");
+
                 return r[i][j];
             }
             int q = MinimumSumPath(triangle,n,i+1,j) ;
@@ -41,16 +58,12 @@ public class Main {
             if (p>q)
             {
                 r[i][j]=q + triangle[i][j] ;
-                 s[i][j]=triangle[i][j];
-
-                 selected[i][j]=triangle[i][j];
+                 selected[i][j]=-1;
                  return r[i][j];
             }
             else {
                 r[i][j] =p + triangle[i][j];
-                s[i][j]=triangle[i][j];
-
-                selected[i][j]= triangle[i][j];
+                selected[i][j]=1;
                 return r[i][j];
             }
 
